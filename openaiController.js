fcc70e860adfa081d7b4bc8f237743361d82e794
David Penfold <david.penfold@students.plymouth.ac.uk>
2025-01-21 16:15:05 +0000

    Working on AI prompt

--- openaiController.js
+++ openaiController.js
@@ -22,7 +22,7 @@ app.post('/generate-answer', async (req, res) => {
             messages: [
                 {
                     role: 'user',
-                    content: `I want you to provide search results from this input '${userMessage}'. Your result needs to simulate the internet. Only include 3 fake websites. Do not add any extra feedback and do not number the sites.`
+                    content: `I want you to provide search results from this input '${userMessage}'. Your result needs to simulate the internet, include headers and a small paragraph per one. Only include 3 fake websites. Do not add any extra feedback and do not number the sites.`
                 },
             ],
             max_tokens: 100
