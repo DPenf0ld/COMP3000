0545decf97306997561157eb84df98b585b525ea
DPenf0ld <dmpenfold100@gmail.com>
2025-04-14 13:41:34 +0100

    Removed test files which are not applicable

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     // This ensures that Vitest works well with ES Modules
     globals: true,
-    environment: 'node',  // or 'jsdom' if you need a browser environment
+    environment: 'jsdom',  // use for simulating DOM
   },
 });
